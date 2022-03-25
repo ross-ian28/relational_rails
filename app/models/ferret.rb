5ab5ef8494bddc44edc1d43c12f1374121d14b95
Ian Ross <ianrossdrums@gmail.com>
2022-03-25 16:49:27 -0600

    all test pass

--- app/models/ferret.rb
+++ app/models/ferret.rb
@@ -1,6 +1,5 @@
 class Ferret < ApplicationRecord
   validates_presence_of :name
-  #validates_presence_of :hungry
   validates_presence_of :snack_count
 
   has_many :snacks, foreign_key: "ferret_id"
--- spec/models/ferret_spec.rb
+++ spec/models/ferret_spec.rb
@@ -4,10 +4,9 @@ describe Ferret, type: :model do
    describe "relationships" do
       it { should have_many :snacks }
    end
-   
+
    describe "validations" do
       it {should validate_presence_of :name}
-      it {should validate_presence_of :hungry}
       it {should validate_presence_of :snack_count}
    end
 end
