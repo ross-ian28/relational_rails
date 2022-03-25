92a901d85f7ca30e2c0d66e135e6221bf7acc9a8
Ian Ross <ianrossdrums@gmail.com>
2022-03-25 16:59:48 -0600

    all tests pass

--- spec/models/ferret_spec.rb
+++ spec/models/ferret_spec.rb
@@ -7,7 +7,6 @@ describe Ferret, type: :model do
 
    describe "validations" do
       it {should validate_presence_of :name}
-      #it {should validate_presence_of :hungry}
       it {should validate_presence_of :snack_count}
    end
 end
