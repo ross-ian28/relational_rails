c91130d38d7c9e50df0632b7072bd03d3c3a8d78
Ian Ross <ianrossdrums@gmail.com>
2022-03-26 11:37:35 -0600

    adds new ferret link

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get '/ferrets', to: "ferrets#index"
+  get '/ferrets/new', to: "ferrets#new"
   get '/ferrets/:id', to: "ferrets#show"
+  post '/ferrets', to: "ferrets#create"
   get '/ferrets/:ferret_id/snacks', to: "ferret_snacks#index"
 
   get '/snacks', to: "snacks#index"
