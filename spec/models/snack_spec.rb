be7b677396f70ed2658caf62624935938e556aea
Ian Ross <ianrossdrums@gmail.com>
2022-03-28 17:11:44 -0600

    adds ferret safe snacks to snacks index page

--- spec/models/snack_spec.rb
+++ spec/models/snack_spec.rb
@@ -9,4 +9,17 @@ describe Snack, type: :model do
      it {should validate_presence_of :name}
      it {should validate_presence_of :count}
    end
+   describe "instance methods" do
+     describe "#ferret_safe_true" do
+       before :each do
+         @ferret1 = Ferret.create!(name:"Pabu", hungry: true, snack_count: 999)
+         @snack1 = @ferret1.snacks.create!(name: "Peanut butter", ferret_safe: true, count: 30)
+         @snack2 = @ferret1.snacks.create!(name: "Egg yolk", ferret_safe: true, count: 1)
+         @snack3 = @ferret1.snacks.create!(name: "Grapes", ferret_safe: false, count: 5)
+       end
+       it 'returns only true snack objects' do
+         expect(Snack.ferret_safe_true).to eq([@snack1, @snack2])
+       end
+     end
+   end
 end
